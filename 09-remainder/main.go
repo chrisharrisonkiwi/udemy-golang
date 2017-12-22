878cf587a376a55cd01611a0d42dcdf2349a4b19
ChristoKiwi <chris.harrison.kiwi@gmail.com>
2017-12-22 15:32:58 +1300

    Remainders - odd even using a loop (classic modulus demo)

---
+++ 09-remainder/main.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+func main() {
+	x := 13 % 3
+	fmt.Println(x)
+	if x == 1 {
+		fmt.Println("It's Odd")
+	} else {
+		fmt.Println("It's Even")
+	}
+
+	for i := 1; i <= 5; i++ {
+
+		// note: parentheses are optional in Go. You could, if (i%2 == 1) { but why add code...
+		if i % 2 == 1 {
+			fmt.Println("Odd")
+		} else {
+			fmt.Println("Even")
+		}
+	}
+}
