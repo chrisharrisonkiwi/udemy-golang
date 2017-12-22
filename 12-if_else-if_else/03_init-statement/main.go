4912905f87fd6a71b95e16cd562b8b001916a478
ChristoKiwi <chris.harrison.kiwi@gmail.com>
2017-12-22 17:20:59 +1300

    Ifs elses and some interesting assignment and scope behaviours within if statements

---
+++ 12-if_else-if_else/03_init-statement/main.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	b := true
+
+	if food := "Chocolate"; b {
+		fmt.Println(food)
+	}
+
+	// b can be a check
+	// "food" only visible within the if
+
+}
